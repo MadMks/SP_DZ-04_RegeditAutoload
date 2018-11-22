24a5673bbf1e90c9b72623bbb2717bc5328cb0aa
Makc <mks59k@gmail.com>
2018-11-23 00:14:07 +0200

    Создание формы.

--- RegeditAutoload/MainForm.Designer.cs
+++ RegeditAutoload/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RegeditAutoload
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(449, 182);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Настройка автозагрузки";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- RegeditAutoload/Program.cs
+++ RegeditAutoload/Program.cs
@@ -16,7 +16,7 @@ namespace RegeditAutoload
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
