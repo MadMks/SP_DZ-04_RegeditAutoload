9169598e6ff01c5d03ee778d80647669a32daf4f
Makc <mks59k@gmail.com>
2018-11-24 13:47:29 +0200

    Создание формы добавления.

---
+++ RegeditAutoload/Forms/AddForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace RegeditAutoload.Forms
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxParameter = new System.Windows.Forms.TextBox();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.labelParameter = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(33, 143);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "Записать";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(166, 143);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // textBoxParameter
+            // 
+            this.textBoxParameter.Location = new System.Drawing.Point(13, 35);
+            this.textBoxParameter.MaxLength = 20;
+            this.textBoxParameter.Name = "textBoxParameter";
+            this.textBoxParameter.Size = new System.Drawing.Size(249, 21);
+            this.textBoxParameter.TabIndex = 2;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(13, 90);
+            this.textBoxValue.MaxLength = 16383;
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(249, 21);
+            this.textBoxValue.TabIndex = 3;
+            // 
+            // labelParameter
+            // 
+            this.labelParameter.AutoSize = true;
+            this.labelParameter.Location = new System.Drawing.Point(13, 15);
+            this.labelParameter.Name = "labelParameter";
+            this.labelParameter.Size = new System.Drawing.Size(56, 13);
+            this.labelParameter.TabIndex = 4;
+            this.labelParameter.Text = "Параметр";
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(13, 72);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(55, 13);
+            this.labelValue.TabIndex = 5;
+            this.labelValue.Text = "Значение";
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 178);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.labelParameter);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.textBoxParameter);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "AddForm";
+            this.Text = "Добавление параметра";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textBoxParameter;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Label labelParameter;
+        private System.Windows.Forms.Label labelValue;
+    }
+}
